0e6d61201785ee95ec088bd484ffea3bd8d629ba
polygon <polygon@wh2.tu-dresden.de>
2023-11-30 13:27:46 +0100

    Move to 23.11 since everything needed from unstable is there

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Audio Nix packages and modules";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
 
 
