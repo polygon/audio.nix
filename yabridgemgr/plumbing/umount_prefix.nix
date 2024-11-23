65387f52226f7a380675ed728b9c11e776eb60ff
polygon <polygon@wh2.tu-dresden.de>
2024-11-23 02:10:40 +0100

    Use SUID binaries

--- yabridgemgr/plumbing/umount_prefix.nix
+++ yabridgemgr/plumbing/umount_prefix.nix
@@ -3,7 +3,7 @@ writeShellScriptBin "umount_prefix" ''
   echo "Unmounting overlayfs" >&2
   TMPDIR="$HOME/yabridgemgr"
 
-  ${util-linux}/bin/umount $TMPDIR
+  /run/wrappers/bin/umount $TMPDIR
 
   echo "Unmounting squashfs" >&2
   ${util-linux}/bin/umount $TMPDIR/squash
